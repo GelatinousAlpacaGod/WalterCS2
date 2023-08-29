92c3ed1a94beaf9001c34b696c81898c0045acb9
walt <142909836+GelatinousAlpacaGod@users.noreply.github.com>
2023-08-29 14:31:01 +0000

    commiting assignment printing practice

---
+++ printingpractice/printing.java
@@ -0,0 +1,11 @@
+class printing
+{
+    public static void main(String args[])
+    {
+       System.out.println("User's name: alvin chipmunk");
+       System.out.println("");
+       System.out.println(" User's age: 97");
+       System.out.println("");
+       System.out.println("thank you for using user greeter");
+    }
+}

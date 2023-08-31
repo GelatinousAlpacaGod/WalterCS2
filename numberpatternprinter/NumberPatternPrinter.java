471df1107b671b867747812bdef39227a48910b3
walt <142909836+GelatinousAlpacaGod@users.noreply.github.com>
2023-08-31 13:42:36 +0000

    fibonacci pyramid

---
+++ numberpatternprinter/NumberPatternPrinter.java
@@ -0,0 +1,12 @@
+class NumberPatternPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("");
+        System.out.println("hola no hablo english");
+        System.out.println("");
+        System.out.println("0");
+        System.out.println("1 1");
+        System.out.println("2 3 5");
+    }
+}

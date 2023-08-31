df516fb3ae447d348540f88c220d4e95144b7dd9
walt <142909836+GelatinousAlpacaGod@users.noreply.github.com>
2023-08-31 13:30:01 +0000

    schort schtory

---
+++ schortschtory/story.java
@@ -0,0 +1,14 @@
+//walter
+class story
+{
+    public static void main(String args[])
+    {
+        System.out.print("Boogie Wonderland");
+        System.out.println(" ");
+        System.out.print("once upon a time there were ten thousand weirdos called booglings");
+        System.out.println(" ");
+        System.out.print("and they boogied");
+        System.out.println(" ");
+        System.out.print("because if you don't boogie in boogie wonderland you get shot");
+    }
+}

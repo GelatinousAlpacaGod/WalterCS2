75d44aefa0ec5ab65e7d5286463290131b86b8b8
walt <142909836+GelatinousAlpacaGod@users.noreply.github.com>
2023-08-31 14:09:07 +0000

    little birdy

---
+++ printingart/art.java
@@ -0,0 +1,13 @@
+class printing
+{
+    public static void main(String args[])
+    {
+       System.out.println("  _______");
+       System.out.println("  | i  i |");
+       System.out.println("  |   >  |");
+       System.out.println("---      ---");
+       System.out.println("|--      --|");
+       System.out.println("  |      |");
+       System.out.println("  |_|--|_|");
+    }
+}
